44d448da2aa29eb25e669cb5d24d65876fc025db
Nishant Sanjay Kumbhar <nishantsk210@gmail.com>
2023-07-02 11:30:12 +0530

    Perform ActionResult and Status Code in Mobiles.

--- PatanWalks/Controllers/MobilesController.cs
+++ PatanWalks/Controllers/MobilesController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using PatanWalks.Models.Sample;
 
 namespace PatanWalks.Controllers
 {
@@ -7,29 +8,45 @@ namespace PatanWalks.Controllers
     [ApiController]
     public class MobilesController : ControllerBase
     {
+
+        private List<MobileModel> Mobiles = new List<MobileModel>
+        {
+            new MobileModel{ Id = 1, Name = "Redmi Note 10 Pro", Description = "Best in Class.", Price= 19000, Color="Black"},
+            new MobileModel{ Id = 2, Name = "Apple 14 Pro", Description = "New Beginning.", Price= 90000, Color="Product Red"},
+            new MobileModel{ Id = 3, Name = "One Plus 7 Pro", Description = "Never Settle.", Price= 60000, Color="Grey Silver"},
+            new MobileModel{ Id = 4, Name = "Samsung S22 Ultra", Description = "Behind the Moon.", Price= 98000, Color="Glossy Red"},
+            new MobileModel{ Id = 5, Name = "Sony Xperia Pro", Description = "El Classico.", Price= 70000, Color="Matfinish Black"}
+
+        };
+        // https://localhost:7104/api/Mobiles
+        [HttpGet]
+        public ActionResult<IEnumerable<MobileModel>> GetMobiles()
+        {
+            return Mobiles;
+        }
+
         //[HttpGet]
-        //public IActionResult GetMobiles()
+        //public IActionResult GetPrices()
         //{
-            
-        //    List<string> MobileNames = new List<string>();
-        //    MobileNames.Add("Redmi Note 10 Pro");
-        //    MobileNames.Add("Redmi Note 9 Pro");
-        //    MobileNames.Add("Redmi Note 8 Pro");
-        //    MobileNames.Add("Redmi Note 7 Pro");
-        //    MobileNames.Add("Redmi Note 6 Pro");
-        //    return Ok(MobileNames);
+        //    List<int> Prices  = new List<int>();
+        //    Prices.Add(19000);
+        //    Prices.Add(18012);
+        //    Prices.Add(17000);
+        //    Prices.Add(17000);
+        //    Prices.Add(15000);
+        //    return Ok(Prices);
         //}
 
-        [HttpGet]
-        public IActionResult GetPrices()
+        // https://localhost:7104/api/Mobiles/2
+        [HttpGet("{id}")]
+        public ActionResult<MobileModel> Get(int id)
         {
-            List<int> Prices  = new List<int>();
-            Prices.Add(19000);
-            Prices.Add(18012);
-            Prices.Add(17000);
-            Prices.Add(17000);
-            Prices.Add(15000);
-            return Ok(Prices);
+            MobileModel mobile =  Mobiles.FirstOrDefault(m => m.Id == id);
+            if(mobile == null)
+            {
+                return NotFound(new {Messsage = "Mobile Not Found with that ID."});
+            }
+            return Ok(mobile);
         }
 
     }
---
+++ PatanWalks/Models/Sample/MobileModel.cs
@@ -0,0 +1,12 @@
+﻿namespace PatanWalks.Models.Sample
+{
+    public class MobileModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Price { get; set; }
+        public string Color { get; set; }
+
+    }
+}
