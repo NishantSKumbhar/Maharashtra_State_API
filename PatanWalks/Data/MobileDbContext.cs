7c44a61b4daf3e3bbaf0451150e86d8f6df28671
Nishant Sanjay Kumbhar <nishantsk210@gmail.com>
2023-07-09 10:37:48 +0530

    dont know

---
+++ PatanWalks/Data/MobileDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PatanWalks.Models.Sample;
+
+namespace PatanWalks.Data
+{
+    public class MobileDbContext: DbContext
+    {
+        public MobileDbContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
+        {
+
+        }
+
+        // Mobile Table
+        public DbSet<MobileModel> Mobile { get; set; }
+    }
+}
