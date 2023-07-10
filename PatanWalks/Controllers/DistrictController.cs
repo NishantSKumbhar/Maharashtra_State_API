440b18756167fb268f85179e6f96fcb5f0be55c4
Nishant Sanjay Kumbhar <nishantsk210@gmail.com>
2023-07-10 07:40:26 +0530

    still in crud

--- PatanWalks/Controllers/DistrictController.cs
+++ PatanWalks/Controllers/DistrictController.cs
@@ -4,6 +4,7 @@ using Microsoft.DotNet.Scaffolding.Shared.Messaging;
 using Microsoft.EntityFrameworkCore;
 using PatanWalks.Data;
 using PatanWalks.Models.Domain;
+using PatanWalks.Models.DTO;
 
 namespace PatanWalks.Controllers
 {
@@ -20,23 +21,93 @@ namespace PatanWalks.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<District>> GetDistricts()
         {
+            // Make DTO
+            var districtDTO = new List<DistrictDTO>();
+
             var districts = dbContext.Districts.ToList();
-            return Ok(districts);
+
+
+            // Map the DTO
+            foreach (var dis in districts)
+            {
+                districtDTO.Add(new DistrictDTO()
+                {
+                    Id = dis.Id,
+                    Name = dis.Name,
+                    Description = dis.Description,
+                    AreaInSqKm = dis.AreaInSqKm,
+                    DistrictImageUrl = dis.DistrictImageUrl,
+                    DivisionId = dis.DivisionId,
+                    PopulationId = dis.PopulationId
+                });
+            }
+
+            // Return the DTO
+
+            return Ok(districtDTO);
         }
 
         [HttpGet]
         [Route("{id}")]
-        public ActionResult<District> GetDistrictById([FromRoute]Guid id)
+        public ActionResult<DistrictDTO> GetDistrictById([FromRoute]Guid id)
         {
-            //var district = dbContext.Districts.Find(id);
-            // Above Find Method will take only primary key
-            var district = dbContext.Districts.FirstOrDefault(x => x.Id == id);
+           
 
+
+            var district = dbContext.Districts.Find(id);
+
+            // Above Find Method will take only primary key
+            //var district = dbContext.Districts.FirstOrDefault(x => x.Id == id);
+
+            
             if (district == null) 
             {
                 return NotFound(new { Message = "District with that ID not Found."});
             }
-            return Ok(district);
+
+            var districtDTO = new DistrictDTO()
+            {
+                Id = district.Id,
+                Name = district.Name,
+                Description = district.Description,
+                AreaInSqKm = district.AreaInSqKm,
+                DistrictImageUrl = district.DistrictImageUrl,
+                DivisionId = district.DivisionId,
+                PopulationId = district.PopulationId
+            };
+
+            return Ok(districtDTO);
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] DistrictDTO addDistrictDTO) 
+        {
+            var districtModel = new District
+            {
+                
+                Name = addDistrictDTO.Name,
+                Description = addDistrictDTO.Description,
+                DistrictImageUrl = addDistrictDTO.DistrictImageUrl,
+                AreaInSqKm = addDistrictDTO.AreaInSqKm,
+                DivisionId = addDistrictDTO.DivisionId,
+                PopulationId = addDistrictDTO.PopulationId
+            };
+
+            dbContext.Districts.Add(districtModel);
+            dbContext.SaveChanges();
+
+            var districtDto = new DistrictDTO
+            {
+                Id = districtModel.Id,
+                Name = districtModel.Name,
+                Description = districtModel.Description,
+                DistrictImageUrl = districtModel.DistrictImageUrl,
+                AreaInSqKm = districtModel.AreaInSqKm,
+                DivisionId = districtModel.DivisionId,
+                PopulationId = districtModel.PopulationId
+            };
+
+            return CreatedAtAction(nameof(GetDistrictById), new { id = districtDto.Id }, districtDto);
         }
     }
 }
---
+++ PatanWalks/Models/DTO/DistrictDTO.cs
@@ -0,0 +1,20 @@
+﻿using PatanWalks.Models.Domain;
+
+namespace PatanWalks.Models.DTO
+{
+    public class DistrictDTO
+    {
+        // EXPOSE properties which you want to expose to client
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double AreaInSqKm { get; set; }
+        public string? DistrictImageUrl { get; set; }
+        public Guid DivisionId { get; set; }
+        public Guid PopulationId { get; set; }
+
+        // Navigation Property
+        public Division Division { get; set; }
+        public Population Population { get; set; }
+    }
+}
